950324c34a9503af5330ff07122df11e1f03acc5
ndgigliotti <ndgigliotti@gmail.com>
2021-02-13 00:43:25 -0500

    Add some docstrings

--- tasks.py
+++ tasks.py
@@ -11,12 +11,14 @@ logger = logging.getLogger(__name__)
 
 
 def _log_strings(strings: list, level=logging.INFO):
+    """Pretty print a list of strings to the log."""
     strings = pformat(strings).split("\n")
     for string in strings:
         logger.log(level, string)
 
 
 def load_images(pool, entries):
+    """Load images from paths in `entries`."""
     start = time.perf_counter()
     images = pool.map(ski.io.imread, [x.path for x in entries])
     logger.info("Loaded %i images:", len(images))
@@ -27,6 +29,7 @@ def load_images(pool, entries):
 
 
 def check_border(pool, images, entries):
+    """Run the white border test, then remove images that fail (in place)."""
     start = time.perf_counter()
     test_results = pool.map(core.white_border_test, images)
     logger.info("%i of %i images have white border.",
@@ -48,6 +51,7 @@ def check_border(pool, images, entries):
 
 
 def process_images(pool, func, images, entries):
+    """Erase the white backgrounds from `images` and return the new images."""
     start = time.perf_counter()
     images = pool.map(func, images)
     logger.info("Erased white background from %i images:", len(images))
@@ -58,6 +62,7 @@ def process_images(pool, func, images, entries):
 
 
 def save_images(pool, dst, images, entries):
+    """Save `images` to disk in `dst` using the filenames in `entries`."""
     start = time.perf_counter()
     fnames = [os.path.splitext(x.name)[0] + ".png" for x in entries]
     fpaths = [os.path.join(dst, x) for x in fnames]
